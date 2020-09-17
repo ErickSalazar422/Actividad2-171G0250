ca9fe93cd7e5e0c6eadec1b5dd60e46df99b4f56
ErickSalazar422 <71106495+ErickSalazar422@users.noreply.github.com>
2020-09-17 18:24:25 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad2U2_171G0250.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad2U2_171G0250.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            villancicosContext context = new villancicosContext();
+            var villancicos = context.Villancico.OrderByDescending(x => x.Nombre);
+            return View(villancicos);
+
+        }
+        public IActionResult villancico(int? id)
+        {
+            if (id == null)
+            { return RedirectToAction("Index"); }
+
+            villancicosContext context = new villancicosContext();
+            var villancicos = context.Villancico
+              .FirstOrDefault(x => x.Id == id);
+            if (villancicos == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+
+            {
+                return View(villancicos);
+            }
+        }
+
+      
+    }
+}
---
+++ Models/Villancico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad2U2_171G0250.Models
+{
+    public partial class Villancico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Compositor { get; set; }
+        public int? Anyo { get; set; }
+        public string Letra { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
